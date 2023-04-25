3ddc91935602145ce3d493a7aaab8fc33a912a7a
olga_lihaia <olga.l@usenergysolutions.us>
2023-04-25 18:41:20 +0300

    Create single post page

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>
-    <section>
-      <h1>Title of the Post</h1>
+  <div class="single-post-page">
+    <section class="post">
+      <h1 class="post-title">Title of the Post</h1>
+      <div class="post-details">
+        <div class="post-detail">Last updates on XXX</div>
+        <div class="post-detail">Written by NAME</div>
+      </div>
+      <p>Content of the post</p>
     </section>
-    <section></section>
+    <section class="post-feedback">
+      <p>
+        Let me know what you think about the post, send a mail to
+        <a href="mailto:olga.lihaia@gmail.com">
+          olga.lihaia@gmail.com
+        </a>
+      </p>
+    </section>
   </div>
 </template>
 
