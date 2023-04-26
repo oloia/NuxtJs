1eba62fbd96f97270032364c529fa08c030c02d7
olga_lihaia <olga.l@usenergysolutions.us>
2023-04-26 15:06:46 +0300

    Small fix

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import PostPreview from "@/components/Posts/PostPreview";
+import PostPreview  from "../components/Posts/PostPreview";
 
 export default {
   components: {
